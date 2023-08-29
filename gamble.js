e1bc9196531282f9231d69cfce88039443b2e5d6
NMarko95 <98682412+NMarko95@users.noreply.github.com>
2023-08-29 06:42:51 +0200

    Add files via upload

--- gamble.js
+++ gamble.js
@@ -1,205 +1,215 @@
-const mainContainer = document.querySelector(".main-container");
-const mainCanvas = document.querySelector(".main-canvas");
-const historyCanvas = document.querySelector(".history-canvas");
+function gamble(gambleAmount) {
+  const mainContainer = document.querySelector(".main-container");
+  const mainCanvas = document.querySelector(".main-canvas");
+  const historyCanvas = document.querySelector(".history-canvas");
+  const gameContainer = document.querySelector(".game");
 
-const c = mainCanvas.getContext("2d");
-const historyC = historyCanvas.getContext("2d");
+  const context = mainCanvas.getContext("2d");
+  const historyContext = historyCanvas.getContext("2d");
 
-let currentTime = Date.now(),
-  lastRender = Date.now();
+  const imgDim = {
+    width: 686,
+    height: 976,
+  };
 
-let animationId;
+  mainCanvas.width = imgDim.width;
+  mainCanvas.height = imgDim.height;
 
-let blue = "rgb(0, 0, 255)",
-  red = "rgb(255, 0, 0)";
+  historyCanvas.width = imgDim.width * 4;
+  historyCanvas.height = imgDim.height;
 
-let history = [];
+  let currentTime = Date.now(),
+    lastRender = Date.now();
 
-let currentColor = red;
+  let animationId;
 
-let amount = 50;
-let amountWin = amount * 2;
-let attempts = 5;
+  let black = "rgb(0, 0, 0)",
+    red = "rgb(255, 0, 0)";
 
-let cardPosition = {
-  x: mainCanvas.width / 2 - 75,
-  y: mainCanvas.height / 2 - 125,
-};
+  let history = [];
 
-const text = document.querySelector(".display-text");
-const btns = document.querySelectorAll(".btn");
-const attemptsValue = document.querySelector(".attempts-value");
-attemptsValue.innerHTML = attempts;
-const gambleWinValue = document.querySelector(".gamble-win-value");
-gambleWinValue.innerHTML = amountWin;
-const gambleAmountValue = document.querySelector(".gamble-amount-value");
-gambleAmountValue.innerHTML = amount;
+  let currentColor = red;
 
-let btn = document.querySelector(".btn.red");
-btn.style.backgroundColor = red;
+  let amount = gambleAmount;
+  let amountWin = amount * 2;
+  let attempts = 5;
 
-btn = document.querySelector(".btn.blue");
-btn.style.backgroundColor = blue;
+  const text = document.querySelector(".display-text");
+  const btns = document.querySelectorAll(".btn");
 
-let img = new Image();
-img.src = "cards.jpg";
-
-let containerHeight, containerWidth, newContext;
-
-const aspectRatioScreen = {
-  widthScale: 16,
-  heightScale: 9,
-};
-
-function drawCard(isRed) {
-  c.drawImage(
-    img,
-    isRed ? 26 : img.width / 2 + 5,
-    15,
-    img.width / 2 - 30,
-    img.height - 30,
-    0,
-    0,
-    mainCanvas.width,
-    mainCanvas.height
-  );
-}
-
-function scaleImage(source, scaleFactor) {
-  let temp = document.createElement("canvas");
-  let ctx = temp.getContext("2d");
-  let w = source.height * scaleFactor;
-  temp.width = w;
-  ctx.drawImage(source, 0, 0, w, source.height);
-  return temp;
-}
-
-function drawHistoryCard() {
-  historyC.clearRect(0, 0, historyCanvas.width, historyCanvas.height);
-  history.forEach((card, i) => {
-    historyC.drawImage(
-      img,
-      card === red ? 25 : img.width / 2 + 5,
-      15,
-      img.width / 2 - 30,
-      img.height - 30,
-      ((history.length - 1 - i) * historyCanvas.width) / 4,
-      0,
-      historyCanvas.width / 4.5,
-      historyCanvas.height
-    );
-  });
-}
-
-function starting() {
-  if (lastRender - currentTime >= 100) {
-    if (currentColor === red) {
-      currentColor = blue;
-      drawCard(false);
-    } else {
-      currentColor = red;
-      drawCard(true);
-    }
-    currentTime = Date.now();
-  }
-  lastRender = Date.now();
-  animationId = requestAnimationFrame(starting);
-}
-
-function addToHistory(color) {
-  history.push(color);
-  drawHistoryCard();
-}
-
-function checkWin(color) {
-  if (currentColor === color) {
-    attempts -= 1;
-    text.innerHTML = "WIN";
-  } else {
-    attempts = 5;
-    amount = 50;
-    text.innerHTML = "LOSE";
-  }
-  amountWin = amount * 2;
+  const attemptsValue = document.querySelector(".attempts-value");
   attemptsValue.innerHTML = attempts;
-  gambleAmountValue.innerHTML = amount;
+  const gambleWinValue = document.querySelector(".gamble-win-value");
   gambleWinValue.innerHTML = amountWin;
-  cancelAnimationFrame(animationId);
-  disableSwitch(true);
-  addToHistory(currentColor);
-  setTimeout(() => {
-    if (attempts === 0) {
-      amount = 50;
-      attempts = 5;
-      console.log("You won 5 times in a row!");
-    } else {
-      amount = amountWin;
-    }
-    text.innerHTML = "";
-    disableSwitch(false);
-    starting();
-  }, 2000);
-}
+  const gambleAmountValue = document.querySelector(".gamble-amount-value");
+  gambleAmountValue.innerHTML = amount;
 
-function disableSwitch(condition) {
-  btns.forEach((btn) => {
-    btn.disabled = condition;
-  });
-}
+  let btn = document.querySelector(".btn.red");
+  btn.style.backgroundColor = red;
 
-function proportionalScale(isLandscape) {
-  containerHeight =
-    parseInt(mainContainer.style.height.split("p")[0]) || innerHeight;
-  containerWidth =
-    parseInt(mainContainer.style.width.split("p")[0]) || innerWidth;
-  if (isLandscape) {
-    if (containerHeight + 1 >= innerHeight && innerWidth >= containerWidth) {
-      mainContainer.style.height = `${innerHeight}px`;
-      mainContainer.style.width = `${
-        (parseFloat(innerHeight) * aspectRatioScreen.widthScale) /
-        aspectRatioScreen.heightScale
-      }px`;
-    } else {
-      mainContainer.style.width = `${innerWidth}px`;
-      mainContainer.style.height = `${
-        (parseFloat(innerWidth) * aspectRatioScreen.heightScale) /
-        aspectRatioScreen.widthScale
-      }px`;
-    }
-  } else {
-    if (containerWidth + 1 >= innerWidth && innerHeight >= containerHeight) {
-      mainContainer.style.width = `${innerWidth}px`;
-      mainContainer.style.height = `${
-        (parseFloat(innerWidth) * aspectRatioScreen.widthScale) /
-        aspectRatioScreen.heightScale
-      }px`;
-    } else {
-      mainContainer.style.height = `${innerHeight}px`;
-      mainContainer.style.width = `${
-        (parseFloat(innerHeight) * aspectRatioScreen.heightScale) /
-        aspectRatioScreen.widthScale
-      }px`;
-    }
+  btn = document.querySelector(".btn.black");
+  btn.style.backgroundColor = black;
+
+  let imgBlack = new Image();
+  imgBlack.src = "black.png";
+
+  let imgRed = new Image();
+  imgRed.src = "red.png";
+
+  let currentImg;
+
+  function drawCard(isRed) {
+    if (isRed) currentImg = imgRed;
+    else currentImg = imgBlack;
+    context.drawImage(
+      currentImg,
+      0,
+      0,
+      currentImg.width,
+      currentImg.height,
+      0,
+      0,
+      mainCanvas.width,
+      mainCanvas.height
+    );
   }
+
+  function drawHistoryCard() {
+    historyContext.clearRect(0, 0, historyCanvas.width, historyCanvas.height);
+    history.forEach((card, i) => {
+      currentImg = card === red ? imgRed : imgBlack;
+      historyContext.drawImage(
+        currentImg,
+        0,
+        0,
+        currentImg.width,
+        currentImg.height,
+        ((history.length - 1 - i) * historyCanvas.width) / 4,
+        0,
+        historyCanvas.width / 5,
+        historyCanvas.height
+      );
+    });
+  }
+
+  function starting() {
+    if (lastRender - currentTime >= 80) {
+      if (currentColor === red) {
+        currentColor = black;
+        drawCard(false);
+      } else {
+        currentColor = red;
+        drawCard(true);
+      }
+      currentTime = Date.now();
+    }
+    lastRender = Date.now();
+    animationId = requestAnimationFrame(starting);
+  }
+
+  function addToHistory(color) {
+    history.push(color);
+    drawHistoryCard();
+  }
+
+  function checkWin(color) {
+    if (currentColor === color) {
+      attempts -= 1;
+      text.innerHTML = "WIN";
+    } else {
+      attempts = 5;
+      amount = gambleAmount;
+      text.innerHTML = "LOSE";
+    }
+    amountWin = amount * 2;
+    attemptsValue.innerHTML = attempts;
+    gambleAmountValue.innerHTML = amount;
+    gambleWinValue.innerHTML = amountWin;
+    cancelAnimationFrame(animationId);
+    disableSwitch(true);
+    addToHistory(currentColor);
+    setTimeout(() => {
+      if (attempts === 0) {
+        amount = gambleAmount;
+        attempts = 5;
+        console.log("You won 5 times in a row!");
+      } else {
+        amount = amountWin;
+      }
+      text.innerHTML = "";
+      disableSwitch(false);
+      starting();
+    }, 2000);
+  }
+
+  function disableSwitch(condition) {
+    btns.forEach((btn) => {
+      btn.disabled = condition;
+    });
+  }
+
+  let aspectRatioScreen = {
+    widthScale: 16,
+    heightScale: 9,
+  };
+
+  const ASPECT_RATIO_MAIN = 16 / 9;
+
+  function resize(width, height, aspect) {
+    if (width > height) {
+      if (onloadScale > aspect) {
+        gameHeight = height;
+        gameWidth =
+          (gameHeight * aspectRatioScreen.widthScale) /
+          aspectRatioScreen.heightScale;
+      } else {
+        gameWidth = width;
+        gameHeight =
+          (gameWidth * aspectRatioScreen.heightScale) /
+          aspectRatioScreen.widthScale;
+      }
+    } else {
+      if (onloadScale > 1 / aspect) {
+        gameHeight = outerHeight;
+        gameWidth =
+          (gameHeight * aspectRatioScreen.heightScale) /
+          aspectRatioScreen.widthScale;
+      } else {
+        gameWidth = outerWidth;
+        gameHeight =
+          (gameWidth * aspectRatioScreen.widthScale) /
+          aspectRatioScreen.heightScale;
+      }
+    }
+    resizeElementsMain();
+  }
+
+  function resizeElementsMain() {
+    mainContainer.style.width = gameWidth + "px";
+    mainContainer.style.height = gameHeight + "px";
+  }
+
+  let onloadScale;
+
+  const buttons = document.querySelectorAll(".btn");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      checkWin(button.style.backgroundColor);
+    });
+  });
+
+  animationId = requestAnimationFrame(starting);
+
+  addEventListener("load", () => {
+    drawCard(currentColor);
+    onloadScale = innerWidth / innerHeight;
+    resize(innerWidth, innerHeight, ASPECT_RATIO_MAIN);
+  });
+
+  window.onresize = function () {
+    onloadScale = innerWidth / innerHeight;
+    resize(innerWidth, innerHeight, ASPECT_RATIO_MAIN);
+  };
 }
 
-const buttons = document.querySelectorAll(".btn");
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    checkWin(button.style.backgroundColor);
-  });
-});
-
-animationId = requestAnimationFrame(starting);
-
-addEventListener("load", () => {
-  drawCard(currentColor);
-  proportionalScale(innerWidth > innerHeight);
-});
-
-let scaling;
-
-window.onresize = function () {
-  clearTimeout(proportionalScale(innerWidth > innerHeight));
-  scaling = setTimeout(proportionalScale(innerWidth > innerHeight), 100);
-};
+gamble(100);
